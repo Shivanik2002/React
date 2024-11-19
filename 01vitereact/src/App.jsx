9579cc2a42ccaada64c9a6d7b02732de11fbdfcd
Shivani <shivanitawny@gmail.com>
2024-11-19 10:44:18 +0000

    React basic project using vite

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,14 @@
+import Chai from "./chai"
+
+function App() {
+  const username = "shivani"
+  return (
+    <>
+    <Chai />
+    <h1>React with vite {username} </h1>
+    <p>test para</p>
+    </>
+  )
+}
+
+export default App
